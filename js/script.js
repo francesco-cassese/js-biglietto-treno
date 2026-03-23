95e3056f2b18ef5ecdb3c8403964db01f40c19a8
Francesco Cassese <francescocassese9@gmail.com>
2026-03-23 15:14:06 +0100

    Fix: Sostituito il parseInt con il parceFloat sulla distanza dei km e aggiunto il calcolo per il prezzo base del biglietto

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@
 
 const passengerName = prompt("Inserisci qui il tuo nome");
 const passengerAge = prompt("Inserisci la tua età");
-const distanceInKm = parseInt( prompt("Inserisci qui i KM da percorrere"));
+const distanceInKm = parseFloat( prompt("Inserisci qui i KM da percorrere"));
 
+/*
+    =======================================
+    CALCOLO PREZZO BASE DEL BIGLIETTO
+    =======================================
+*/
+
+const basePrice = distanceInKm * 0.21;
 
