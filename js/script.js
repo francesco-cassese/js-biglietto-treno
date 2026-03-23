9eb98bb967cf4fe4d1510fa09d8ab9af2f8a1302
Francesco Cassese <francescocassese9@gmail.com>
2026-03-23 15:59:49 +0100

    Aggiunte condizionali, il parseInt sulla variabile dell'età  e stampa finale

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 */ 
 
 const passengerName = prompt("Inserisci qui il tuo nome");
-const passengerAge = prompt("Inserisci la tua età");
+const passengerAge = parseInt( prompt("Inserisci la tua età") );
 const distanceInKm = parseFloat( prompt("Inserisci qui i KM da percorrere"));
 
 /*
@@ -17,4 +17,38 @@ const distanceInKm = parseFloat( prompt("Inserisci qui i KM da percorrere"));
 */
 
 const basePrice = distanceInKm * 0.21;
+
+
+/*
+    =======================================
+    CONDIZIONALI
+    =======================================
+*/
+
+let totalPrice = 0;
+
+if(passengerAge < 18){
+
+    totalPrice = basePrice * 0.8;
+
+} else if (passengerAge > 65){
+
+    totalPrice = basePrice * 0.6;
+
+} else {
+    
+    totalPrice = basePrice;
+}
+
+/*
+    =======================================
+    CONVERSIONE E STAMPA FINALE
+    =======================================
+*/
+
+console.log(` 
+    Nome passeggero: ${passengerName}
+    Età: ${passengerAge} anni 
+    Costo Biglietto: ${totalPrice.toFixed(2)}€ `);
+
 
