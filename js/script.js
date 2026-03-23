6c146a347abeee100ef7c424d86c02c724d02a69
Francesco Cassese <francescocassese9@gmail.com>
2026-03-23 14:52:17 +0100

    Dichiarazione e assegnazione variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+/*
+    =======================================
+    DICHIARAZIONI E ASSEGNAZIONI VARIABILI
+    =======================================
+*/ 
+
+const passengerName = prompt("Inserisci qui il tuo nome");
+const passengerAge = prompt("Inserisci la tua età");
+const distanceInKm = prompt("Inserisci qui i KM da percorrere")
+
